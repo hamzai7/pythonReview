08cd1d078d5865e25dfa5243ccecb2e1962168e8
Hamza Iqbal <56775151+hamzai7@users.noreply.github.com>
2021-03-20 20:46:15 -0500

    fixed small mistakes
    unused parameter, naming convention, and typo

--- nameLetterCount.py
+++ nameLetterCount.py
@@ -1,14 +1,14 @@
 # This program asks for your name and returns the length
 
-print("Hello! Plase enter your name: ")
+print("Hello! Please enter your name: ")
 userName = input()
 
 print("Hi " + userName + ", it is nice to meet you!")
 
 
-def countName(count):
+def count_name():
     count = str(len(userName))
     return count
 
 
-print("Your name has " + countName(userName) + " letters in it!")
+print("Your name has " + count_name() + " letters in it!")
